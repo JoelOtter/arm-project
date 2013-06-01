8752fb5f3bdff56c114281ba32883f4906fc05eb
JoelOtter <joel.auterson@googlemail.com>
2013-06-01 22:14:31 +0100

    Added binary writer to the assembler. Puts a list of chars into a file, in little-endian order, though sometimes puts in a row of extra zeroes at the end. I'm using 00000000 to define EOF, though this isn't ideal.

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,38 @@
 #include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+void writeBinary(char *path, char *write){
+    FILE *fp;
+    int k = 3;
+    int zeroHit = 0;
+    int zero = 0;
+
+    fp = fopen(path, "wb");
+    while(1) {
+        if (write[k] == 0){
+            fwrite(&zero, sizeof(char), 1, fp);
+            zeroHit = 1;
+        }
+        else {
+            fwrite(&write[k], sizeof(write[k]), 1, fp);
+        }
+        if (k % 4 == 0 && zeroHit) break;
+        else if (k % 4 == 0) k += 7;
+        else --k;
+    }
+}
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+    assert(argc == 3);
+
+    char *srcpath = argv[1];
+    char *destpath = argv[2];
+
+    char *toWrite = malloc(4 * sizeof(char));
+    toWrite[0] = 1;
+    toWrite[1] = 2;
+    toWrite[2] = 3;
+    toWrite[3] = 4;
+    writeBinary(destpath, toWrite);
 }
