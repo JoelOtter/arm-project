a08211db6c87b80f3e4a6bcee72406eb6429e513
Jack Thorp <jackthorp13@gmail.com>
2013-06-06 14:24:20 +0100

    Started working on branch for assembly. Fixed MakeFile to also do assemble

--- src/ass_branch.c
+++ src/ass_branch.c
@@ -1,9 +1,17 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
 uint32_t ass_branch(char *mnemonic, char *rest){
     
     uint32_t condition;
     uint32_t offset;
+    char suffix[2];
+    suffix[0] = mnemonic[1];
+    suffix[1] = mnemonic[2];
+    if (!strcmp(suffix, "ne")) {
+        cond = 
 
 
 }
