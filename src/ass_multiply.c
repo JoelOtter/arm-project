1ec0190d9e96f5512da1246915efe7b72c64f6ee
Zuhayr Chagpar <zuhayrchagpar@gmail.com>
2013-06-06 01:52:07 +0100

    Multiply for assembly done. Briefly tested. Main left in for testing later.

---
+++ src/ass_multiply.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "library.h"
+
+uint32_t assMultiply(char *mnemonic, char *rest) {
+
+    char Rd[2], Rm[2], Rs[2], Rn[2];
+    uint32_t RdI, RmI, RsI, RnI;
+
+    uint32_t result = (111 << 29) | (1 << 4) | (1 << 7);
+
+    if (mnemonic == "mul") {
+        sscanf(rest, "%[^','],%[^','],%s", Rd, Rm, Rs);
+        RnI = 0;
+    } else {
+        sscanf(rest, "%[^','], %[^','], %[^','],%s", Rd, Rm, Rs, Rn);
+        result |= (1 << 21);
+        RnI = regFromString(Rn);
+    }
+
+    RdI = regFromString(Rd);
+    RmI = regFromString(Rm);
+    RsI = regFromString(Rs);
+
+    result |= (RdI << 16) | (RnI << 12) | (RsI << 8) | (RmI & mask4);
+
+    //printf("%x\n", result);
+
+    return result;
+}
+
+/*
+int main(int argc, char const *argv[])
+{
+    return assMultiply("mla", "r1,r2,r3,r4");
+}
+*/
